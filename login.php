d3314e695f6113065396ebf3fd8fcf9135961018
BenjaminCitrin <benjamin.citrin@gmail.com>
2017-04-01 17:16:32 -0400

    added login page

---
+++ login.php
@@ -0,0 +1,28 @@
+
+<?php include("top.php");?>
+<?php include("header.php");?>
+<script> 
+ $(document).ready(function(){
+    $("#studentEmail").change(function(){
+        alert($("#studentEmail").val());
+    });
+});
+ </script>
+
+<form name ='signUpForm' action='login.php'>
+
+    <legend>Login</legend>
+
+    <label for ='studentEmail'> Enter Your Username </label>
+    <input placeholder ='Enter your email' type="text" id="studentEmail" name="txtStudentEmail" value="@uvm.edu"></input>
+
+    <label for ='studentEmail'> Enter Your Password </label>
+    <input placeholder ='Enter your email' type="text" id="studentEmail" name="txtStudentEmail" value="@uvm.edu"></input>
+
+    <button type ='submit' id='submitLogin' onclick='validateEmail()'> </button>
+
+	<label>Not registered? Please <a href="signUp.php">sign-up</a>!</label>
+</form>
+
+
+<?php include("footer.php"); ?>
