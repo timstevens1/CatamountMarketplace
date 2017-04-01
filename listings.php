dd02f43876943fa2f865c607b04b86e3bcc7828a
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 12:02:51 -0400

    another update to landing page

--- listings.php
+++ listings.php
@@ -2,7 +2,32 @@
 <?php include("top.php");?>
 <?php include("header.php");?>
 <div class = "listingWrap">
-    
+    <!--search bar-->
+   <div "searchBarWrap">
+    <form class="searchListingsPage">
+  <input type="text" name="search" placeholder="Search..">
+</form>
+   </div>
+   <!--wrap for listing items-->
+   <div class="itemsListWrap">
+       <!--an item-->
+    <div class= "listingItemContainer">
+    Listing Item Information
+    </div>
+
+        <div class= "listingItemContainer">
+    Listing Item Information
+    </div>
+
+        <div class= "listingItemContainer">
+    Listing Item Information
+    </div>
+
+        <div class= "listingItemContainer">
+    Listing Item Information
+    </div>
+
+   </div>
 </div>
 
 <?php include("footer.php"); ?>
