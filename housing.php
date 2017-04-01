61feee12c70b0d414d2eba353d98612d6ec92b09
lmross <lmross@uvm.edu>
2017-04-01 18:18:11 -0400

    Update housing.php

--- housing.php
+++ housing.php
@@ -3,7 +3,6 @@
 
 <div class="housingPageWrap">
 
-  <div class='images'>
 <a class="apartments" href="listings.php">
 <div>
 <h1>Apartment</h1>
@@ -21,6 +20,7 @@
 <h1>Redstone Lofts</h1>
 </div>
 </a>
-  </div>
+
 </div>
+
 <?php include("footer.php"); ?>
