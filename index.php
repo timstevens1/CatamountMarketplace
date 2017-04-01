4e0a2612faa93199127419a9d9a16cc69a14d4d7
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 09:21:44 -0400

    created a top.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
+<?php include"top.php"?>
+
 <body>
 	<table>
 		<tr>
