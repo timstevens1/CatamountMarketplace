787d2e30bb9870aa47fee557b6d43437cb3d804d
CamWeston <ckweston@uvm.edu>
2017-04-01 14:39:25 -0400

    Update top.php

--- top.php
+++ top.php
@@ -1,4 +1,30 @@
 <!DOCTYPE html>
+<?php    
+print "<!-- require Database.php -->";
+       include ('constants.php');
+       require ('database.php');
+    
+        print "<!-- make Database connections -->"; 
+        $dbName = 'CKWESTON_CATLIST';
+        
+        $dbUserName = 'ckweston' . '_reader';
+        $whichPass = "r"; //flag for which one to use.
+        $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
+        
+       
+        
+        $dbUserName = 'ckweston' . '_writer';
+        $whichPass = "w";
+        $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
+        
+        
+        
+        $dbUserName = 'ckweston' . '_admin';
+        $whichPass = "a";
+        $thisDatabaseAdmin = new Database($dbUserName, $whichPass, $dbName);
+    
+        print "<!-- Database connections successfull -->"; 
+        ?>
 <html>
 <head>
 	<title>Catamount Marketplace</title>
