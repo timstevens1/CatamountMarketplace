b355237dd2560c34d69ae9f8db074e4825f793f6
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 11:33:22 -0400

    updated top for j query

--- top.php
+++ top.php
@@ -13,5 +13,6 @@
 	<link rel="stylesheet" href="css/footer.css" />
 	<link rel="stylesheet" href="css/settings.css" />
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
 <body>
