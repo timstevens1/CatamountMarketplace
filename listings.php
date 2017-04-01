9c16fa73169234f30833feda210dc956d159aba4
spiz3995 <spiz3995@aim.com>
2017-04-01 17:57:08 -0400

    Add files via upload

--- listings.php
+++ listings.php
@@ -1,47 +1,63 @@
 
 <?php include("top.php");?>
 <?php include("header.php");?>
+
+
+<?php
+
+
+
+
+?>
+
+
+
+<?php include("footer.php"); ?>
+
+<?php
+
+$query	=	'SELECT	pmkPostId FROM tblpost';
+$user =	$thisDatabaseReader->select($query,"",	0,	0,	0,	0,	false,	false);
+
+$length = count($user);
+$pmkArray = array();
+for ($i=0; $i<=$length; $i++){
+
+array_push($pmkArray, $user[$i][0]);
+
+}
+
+
+
+?>
+
+
 <div class = "listingWrap">
    <!--wrap for listing items-->
    <div class="itemsListWrap">
        <!--an item-->
+<?php
+for ($i=0; $i<$length; $i++){
 
-        <div class= "listingItemContainer">
-            <a href="specificItem.php">
-    <img src="images/placeHolder.png" alt="">
-    </a>
-    <h3>Title</h3>
-    <h3>Price</h3>
-    <h3>Location</h3>
-    </div>
+	$postKey = array($pmkArray[$i]);
+	
+	$query	=	'SELECT	fldTitle, fldPrice, fldLocation  FROM tblpost WHERE pmkPostId = ?';
+	//public	function	select($query,	$values	=	"",	$wheres	=	1,	$conditions	=	0,
+	//			$quotes	=	0,	$symbols	=	0,	$spacesAllowed	=	false,	$semiColonAllowed	=	false)
+	$item =	$thisDatabaseReader->select($query,	$postKey,	1,	0,	0,	0,	false,	false);
+	
+	
+	echo "<div class='listingItemContainer'>";
+    echo "<a href='specificItem.php'>";
+	echo "<img src='/pics/". ($i+1) .".jpg' alt=''>";
+	echo "    </a>";
+	echo "    <h3>".$item[0][0]." </h3>";
+	echo "    <h3>".$item[0][1]."</h3>";
+	echo "    <h3>".$item[0][2]."</h3>";
+	echo "    </div>";
+}
+?>
 
-        <div class= "listingItemContainer">
-            <a href="specificItem.php">
-    <img src="images/placeHolder.png" alt="">
-    </a>
-    <h3>Title</h3>
-    <h3>Price</h3>
-    <h3>Location</h3>
-    </div>
-
-        <div class= "listingItemContainer">
-            <a href="specificItem.php">
-    <img src="images/placeHolder.png" alt="">
-    </a>
-    <h3>Title</h3>
-    <h3>Price</h3>
-    <h3>Location</h3>
-    </div>
-
-            <div class= "listingItemContainer">
-            <a href="specificItem.php">
-    <img src="images/placeHolder.png" alt="">
-    </a>
-    <h3>Title</h3>
-    <h3>Price</h3>
-    <h3>Location</h3>
-    </div>
+  </div>
 </div>
-
-<?php include("footer.php"); ?>
-
+	
