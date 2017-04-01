8aa5bec9bd435216786bccfe8808739a2655f18c
lmross <lmross@uvm.edu>
2017-04-01 14:57:14 -0400

    Update createAPost.php

--- createAPost.php
+++ createAPost.php
@@ -2,10 +2,25 @@
 <?php include("top.php");?>
 <?php include("header.php");?>
 
-<form>
-<fieldset class="text">
-                <legend>Create A Post</legend>
+<form class="post">
 
+            <legend>Create A Post</legend>
+
+            <p>
+                    <label for="fldTitle" class="required">Product Name </label>
+                    <input type="text" 
+                           id="txtMisc" 
+                           name="txtMisc"
+                           value="<?php print $misc; ?>"
+                           tabindex="120" 
+                           maxlength="45" 
+                           placeholder=""
+                           <?php if ($miscERROR) print 'class="mistake"'; ?>
+                           onfocus="this.select()" 
+                           >
+            </p>
+
+            <fieldset class="text">
                 <p>             
                     <label for="fldDescription" class="required"> Product Description</label>
                     <textarea id="fldDescription" 
@@ -14,96 +29,70 @@
 
             </fieldset>
 
-                <p>
-                    <label for="txtMisc" class="required">Email 2</label>
-                    <input type="text" 
-                           id="txtMisc" 
-                           name="txtMisc"
-                           value="<?php print $misc; ?>"
-                           tabindex="120" 
-                           maxlength="45" 
-                           placeholder="Enter an alternate email"
-                           <?php if ($miscERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()" 
-                           >
+            <fieldset class="text">
+                <p>             
+                    <label for="fldLocation" class="required"> Product Location</label>
+                    <textarea id="fldLocation" 
+                              tabindex="602" onfocus="this.select()"></textarea>
                 </p>
-
 
             </fieldset>
 
-
-            <!-- ends contact -->
-
-            <fieldset class="checkbox">
-                <legend>What is your favorite piano? <br/> Check all that apply:</legend>
-                <p>
-                    <label><input type="checkbox" 
-                                  id="chkChoco" 
-                                  name="chkChoco" 
-                                  value="Choco"
-                                  <?php if ($choco) print ' checked '; ?>                                      
-                                  tabindex="430"> Spinet</label>
-                </p>
-                <p>
-                    <label><input type="checkbox" 
-                                  id="chkSugar" 
-                                  name="chkSugar" 
-                                  value="Sugar"
-                                  <?php if ($sugar) print ' checked '; ?>                                      
-                                  tabindex="430"> Console</label>
-                </p>
-                <p>
-                    <label><input type="checkbox" 
-                                  id="chkFudge" 
-                                  name="chkFudge" 
-                                  value="Fudge"
-                                  <?php if ($fudge) print ' checked '; ?>                                      
-                                  tabindex="430"> Studio </label>
-                </p>
-            </fieldset>
 
             <fieldset class="radio">
-                <legend>What is your age range?</legend>
+                <legend>What is the product condition?</legend>
                 <p><label><input type="radio" 
-                                 id="radUnder18" 
-                                 name="radAge" 
-                                 value="Under 18"
-                                 <?php if ($age == "Under 18") print 'checked' ?>         
-                                 tabindex="330">Under 18</label></p>
+                                 id="radGood"        
+                                 tabindex="330">Good</label></p>
                 <p>
                     <label><input type="radio" 
-                                  id="rad18-25" 
-                                  name="radAge" 
-                                  value="18-25"
-                                  <?php if ($age == "18-25") print 'checked' ?>                                      
-                                  tabindex="340">18-25</label></p>
+                                  id="rad18-25"                                      
+                                  tabindex="340">Okay</label></p>
                 <p>
                     <label><input type="radio" 
-                                  id="radOver25" 
-                                  name="radAge" 
-                                  value="Over 25"
-                                  <?php if ($age == "Over 25") print 'checked' ?>                                      
-                                  tabindex="340">Over 25</label> </p>
+                                  id="radOver25"                                      
+                                  tabindex="340">Bad</label> </p>
+            </fieldset>
+
+            <fieldset class="text">
+                <p>             
+                    <label for="fldPostDate" class="required">Post Date</label>
+                    <textarea id="fldPostDate" 
+                              tabindex="602" onfocus="this.select()"></textarea>
+                </p>
+
+            </fieldset>
+
+            <fieldset class="text">
+                <p>             
+                    <label for="fldPrice" class="required">Price</label>
+                    <textarea id="fldPrice" 
+                              tabindex="602" onfocus="this.select()"></textarea>
+                </p>
+
+            </fieldset>
+
+            <fieldset class="text">
+                <p>             
+                    <label for="fldCategory" class="required">Category</label>
+                    <textarea id="fldCategory" 
+                              tabindex="602" onfocus="this.select()"></textarea>
+                </p>
+
+            </fieldset>
+
+            <fieldset class="text">
+                <p>             
+                    <label for="fldSubCategory" class="required">SubCategory</label>
+                    <textarea id="fldSubCategory" 
+                              tabindex="602" onfocus="this.select()"></textarea>
+                </p>
+
             </fieldset>
 
 
-            <fieldset  class="listbox">
-                <legend>Newsletter Frequency</legend>
-                <p>
-                    <select id="lstAmount" 
-                            name="lstAmount" 
-                            tabindex="520" >
-                        <option <?php if ($amount == "1") print " selected "; ?>
-                            value="1">Once a week</option>
 
-                        <option <?php if ($amount == "2") print " selected "; ?>
-                            value="2">Twice a week</option>
-
-                        <option <?php if ($amount == "3") print " selected "; ?> 
-                            value="3" >Three times a week</option>
-                    </select></p>
-            </fieldset>
-
+           
 
             <input type="hidden" name="hidUrl" value="https://lmross.w3.uvm.edu/cs142/dev/lab1">
             <input type="hidden" name="hidStyle" value="form.css">
