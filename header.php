7d5d9db1c1e801aeb66b6091340ddf26cecf9eef
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 13:06:54 -0400

    fixed the nav and header after the git issues

--- header.php
+++ header.php
@@ -23,9 +23,27 @@
         $dbUserName = 'ckweston' . '_admin';
         $whichPass = "a";
         $thisDatabaseAdmin = new Database($dbUserName, $whichPass, $dbName);
-        
-
-        
-        
+    
         
         ?> 
+
+
+  <div class="banner">
+      <img id="headerLogo" src="images/uvmheaderlogo.svg" alt="" />		    
+      <a id="profileLink" href="user.php">your profile</a>		     
+        <a id="signUpLink" href="signUp.php">sign up</a>
+  </div> 		  
+
+
+  		  
+<ul class="navBar">		     
+    <li><form class="searchListingsPage">
+        <input type="text" name="search" placeholder="Search..">
+        </form>
+    </li>
+ 
+    <li><a class="active" href="#home">Home</a></li>		        
+    <li><a href="#news">News</a></li>		          
+    <li><a href="#contact">Contact</a></li>		          
+
+</ul>
