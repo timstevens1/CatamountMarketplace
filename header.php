08ff05b95bbabdfda1a47dfc74c25623245906a0
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 12:04:59 -0400

    added settings logo, started on nav bar

--- header.php
+++ header.php
@@ -2,4 +2,12 @@
     <img id="headerLogo" src="images/uvmheaderlogo.svg" alt="" />
     <a id="profileLink" href="user.php">your profile</a>
     <a id="signUpLink" href="signUp.php">sign up</a>
+    <a href="settings.php"> <img id="settingsIcon" src="images/settingsicon.png"> </a>
 </div>
+
+    <ul class="navBar">
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+    </ul>
