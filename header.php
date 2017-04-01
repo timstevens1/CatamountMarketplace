5d41f85c70b436c1d7ce1d7c75bfdf8847867a59
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 13:44:34 -0400

    added back the settings link icon

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 
   <div class="banner">
       <img id="headerLogo" src="images/uvmheaderlogo.svg" alt="" />		    
+      
+      <a id="settingsIcon" href="settings.php"> <img src="images/settingsicon.png"> </a>
       <a id="profileLink" href="user.php">your profile</a>		     
         <a id="signUpLink" href="signUp.php">sign up</a>
   </div> 		  
