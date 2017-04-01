219ee21d26ce338f154838af22d9475e9a1a357d
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 09:41:27 -0400

    First steps of main body

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
 <!DOCTYPE html>
 <?php
-//############################################################################
-// find name in uvm directory      
-function ldapName($uvmID) {
-    if (empty($uvmID))
-        return "no:netid";
+include ("phpFunctions.php");
 
-    $name = "not:found";
-
-    $ds = ldap_connect("ldap.uvm.edu");
-
-    if ($ds) {
-        $r = ldap_bind($ds);
-        $dn = "uid=$uvmID,ou=People,dc=uvm,dc=edu";
-        $filter = "(|(netid=$uvmID))";
-        $findthese = array("sn", "givenname");
-
-        // now do the search and get the results which are stored in $info
-        $sr = ldap_search($ds, $dn, $filter, $findthese);
-        
-        // if we found a match (in this example we should actually always find just one
-        if (ldap_count_entries($ds, $sr) > 0) {
-            $info = ldap_get_entries($ds, $sr);
-            $name = $info[0]["givenname"][0] . " " . $info[0]["sn"][0];
-        }
-    }
-
-    ldap_close($ds);
-
-    return $name;
-}
-$userNetId = "ckweston";
-
-$userName = ldapName($userNetId);
-
-echo($userName);
-
-?><html>
+?>
+<html>
 <?php include("top.php");?>
 
 <body>
 <?php include("header.php");?>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello UVM2 from my local git repo!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+<div class="mainWrap">
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
 </body>
 </html>
