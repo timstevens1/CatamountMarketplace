ed8f075e9de612b06c2f44ee99786c80b9e51f18
lmross <lmross@uvm.edu>
2017-04-01 11:07:04 -0400

    Update user.php

--- user.php
+++ user.php
@@ -1,1 +1,20 @@
+<?php include("top.php");?>
+<?php include("header.php");?>
 
+<div class="mainWrap">
+
+    <div class="userHeader">
+        <div class="profPic">
+                <img></img>
+        </div>
+        
+    </div>
+
+    <div class="userBody">
+        <p>
+            Postings
+        </p>
+    </div>
+
+
+<?php include("footer.php"); ?>
