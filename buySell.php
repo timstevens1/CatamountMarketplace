257f6a24d2b3bb238fbc1994971209ea2bc6f687
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 08:41:45 -0400

    pushing buySell.php

---
+++ buySell.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<div class="sellWrap">
+  <h1>Buy Sell Page</h1>
+</div>
+</body>
+</html>
