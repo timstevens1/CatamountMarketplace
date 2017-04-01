9b7aa56d2de14fbeaee27652944d2d0b901ec1a5
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 11:23:49 -0400

    Post template done

--- post.php
+++ post.php
@@ -7,11 +7,41 @@
         <h1>Picture Holder</h1>
     </div>
     <div>
-        <h1>Main information holder</h1>
+        <h1>Title</h1>
+        <h3>price</h3>
+        <p>Description</p>
+        <a href="">Conctact user button</a>
     </div>
+
     </div>
     <div class="bottomItemWrap">
-    <div></div>
+        <div>
+            Place Holder
+        </div>
+    <div>
+        <p>
+            <span>Location</span>
+            <span>Put Location here</span>
+        </p>
+
+         <p>
+            <span>Date</span>
+            <span>Put Date here</span>
+        </p>
+        <p>
+            <span>Condition</span>
+            <span>Put condition here</span>
+        </p>
+        <p>
+            <span>Category</span>
+            <span> Put category here</span>
+        </p>
+
+                <p>
+            <span>Sub Category</span>
+            <span> Put Sub category here</span>
+        </p>
+    </div>
     </div>
 </div>
 
