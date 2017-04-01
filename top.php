d7c78eb214a77ade51792d4d61fafe39633918ab
lmross <lmross@uvm.edu>
2017-04-01 14:59:42 -0400

    Update top.php
    create a post css

--- top.php
+++ top.php
@@ -53,6 +53,7 @@
 	<link rel="stylesheet" href="css/footer.css" />
 	<link rel="stylesheet" href="css/settings.css" />
 	<link rel="stylesheet" href="css/listings.css" />
+	<link rel="stylesheet" href="css/createAPost.css" />
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
