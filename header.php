2f8e11b1165c879dffd21d9c1d10a07e5f0665fb
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 11:07:27 -0400

    added sign up link next to profile

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <div class="banner">
     <img id="headerLogo" src="images/uvmheaderlogo.svg" alt="" />
     <a id="profileLink" href="user.php">your profile</a>
+    <a id="signUpLink" href="signUp.php">sign up</a>
 </div>
