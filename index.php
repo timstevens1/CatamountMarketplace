69027ee511f1e3cd92568d6cadc506587e7ad24f
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 09:52:18 -0400

    some css and html changes

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <?php
 include ("phpFunctions.php");
-
 ?>
+
 <html>
+
 <?php include("top.php");?>
 
 <body>
 <?php include("header.php");?>
+
 <div class="mainWrap">
 <div></div>
 <div></div>
