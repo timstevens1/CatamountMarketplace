96e70dd1c93a2e092d5956b9963d5bbe977383c8
Liam O'Toole <lotoole@uvm.edu>
2017-04-01 14:46:39 -0400

    post updates

--- post.php
+++ post.php
@@ -1,5 +1,26 @@
 <?php include("top.php");?>
 <?php include("header.php");?>
+<?php
+$postId = array(1);
+$query	=	'SELECT	fnkNetId, fldDescription, fldLocation, fldTitle, fldCondition, fldPostDate, fldPrice,
+fldCategory, fldSubCategory FROM tblPost WHERE pmkPostId = ?';
+//public	function	select($query,	$values	=	"",	$wheres	=	1,	$conditions	=	0,
+//			$quotes	=	0,	$symbols	=	0,	$spacesAllowed	=	false,	$semiColonAllowed	=	false)
+$postContent =	$thisDatabaseReader->select($query,	$postId,	1,	0,	0,	0,	false,	false);
+/*
+if (is_array($postContent)){
+        foreach ($postContent as $c){
+            print "<p>" .$c['fldTitle']. " " .$c['fldLocation']. " " .$c['fldPrice']. " " .$c['fldDescription']. " " .$c['fnkNetId'].
+            " " .$c['fldCondition']. " " .$c['fldCategory']. " " .$c['fldSubCategory'].  " " .$c['fldPostDate']. "</p>" ;
+        }
+    }
+*/
+$netId = array($postContent[0][0]);
+$query	= 'SELECT fldEmail FROM tblUser WHERE pmkNetId = ?';
+//public	function	select($query,	$values	=	"",	$wheres	=	1,	$conditions	=	0,
+//			$quotes	=	0,	$symbols	=	0,	$spacesAllowed	=	false,	$semiColonAllowed	=	false)
+$netId =	$thisDatabaseReader->select($query,	$netId,	1,	0,	0,	0,	false,	false);
+?>
 
 
 <div class="itemWrap">
@@ -8,66 +29,37 @@
         <img src="../images/placeHolder.png" alt="">
     </div>
     <div>
-        <h1>Title</h1>
-        <h3>Price</h3>
-        <p class = "addPadding">Descirption</p>
-        <a class="difButton">Email User</a>
+        <h1><?php print($postContent[0][3]); ?></h1>
+        <h3><?php print($postContent[0][6]); ?></h3>
+        <p class = "addPadding"><?php print($postContent[0][1]); ?></p>
+        <a href="mailto:"<?php print($netId[0][0]); ?>"" class="difButton">Email User</a>
             <!--general info div-->
-        <!--<div class="genInfoWrap">-->
+        <div class="genInfoWrap">
         <p style="margin-top: 20px;" class="genInfoP">
             <span>Location</span>
-            <span>Red stone </span>
+            <span><?php print($postContent[0][2]); ?> </span>
         </p>
 
           <p class="genInfoP">
             <span>Date</span>
-            <span>12/16/16 </span>
+            <span><?php print($postContent[0][5]); ?></span>
         </p>
          <p class="genInfoP">
             <span>Condition</span>
-            <span>Great </span>
+            <span><?php print($postContent[0][4]); ?></span>
         </p>
          <p class="genInfoP">
             <span>Category</span>
-            <span>Cool Things </span>
+            <span><?php print($postContent[0][7]); ?></span>
         </p>
 
                  <p class="genInfoP">
             <span>Sub Category</span>
-            <span>Fun Things </span>
-        </p>
-    <!--</div>-->
-    </div>
-    </div>
-    <!--<div class="bottomItemWrap">
-        <div>
-            Place Holder
-        </div>
-    <div>
-        <p>
-            <span>Location</span>
-            <span>Red stone </span>
-        </p>
-
-         <p>
-            <span>Date</span>
-            <span>12/16/16 </span>
-        </p>
-        <p>
-            <span>Condition</span>
-            <span>Great </span>
-        </p>
-        <p>
-            <span>Category</span>
-            <span>Cool Things </span>
-        </p>
-
-                <p>
-            <span>Sub Category</span>
-            <span>Fun Things </span>
+            <span><?php print($postContent[0][8]); ?></span>
         </p>
     </div>
-    </div>-->
+    </div>
+    </div>
 </div>
 
 <?php include("footer.php"); ?>
