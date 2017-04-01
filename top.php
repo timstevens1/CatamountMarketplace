00f9afb11768968aa4d25c4decde0646bd047b42
Timmy Guyon <tguyon@uvm.edu>
2017-04-01 10:03:06 -0400

    making separate stylesheet  for header

--- top.php
+++ top.php
@@ -1,5 +1,6 @@
 <head>
 	<title>Catamount Marketplace</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/header.css" />
 </head>
